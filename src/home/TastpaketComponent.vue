05d0aa77045a998ab560465be7f30b517843edf2
TriumfM <triumf_96@hotmail.com>
2019-06-06 01:38:47 +0200

    html structure

---
+++ src/home/TastpaketComponent.vue
@@ -0,0 +1,51 @@
+<template class="menu__content">
+  <div class="lil__container">
+    <div class="lil__entry">
+      <img class="lil__entry--certificate" src=""/>
+      <div class="lil__entry--discover">
+        <span class="lil__entry--discover-label">Entdecke unsere Hautfreundlichkeit</span>
+        <button class="lil__button lil__entry--discover-button">Jetzt Deine Größe wählen</button>
+      </div>
+    </div>
+    <div class="lil__main-product">
+      <div class="lil__main-product--photo">
+        <img class="lil__main-product--img" src=""/>
+      </div>
+      <div class="lil__main-product--description">
+        <span class="lil__description--title"></span>
+        <div class="lil__description--buttons"></div>
+        <div class="lil__description--content"></div>
+        <button class="lil__button lil__main-product--button">IN DEN WARENKORB LEGEN</button>
+      </div>
+    </div>
+    <div class="lil__functions">
+      <h2 class="lil__functions--title">Wie es funktioniert</h2>
+      <div class="lil__functions--items">
+        <div class="lil__item">
+          <img class="lil__item--img" src=""/>
+          <span class="lil__item--title">DEIN TESTPAKET</span>
+          <span class="lil__item--description">
+            Wir schenbken Dir die Produkte,
+            du übernimmst nur das Porto
+          </span>
+          <a class="lil__item--link"></a>
+        </div>
+      </div>
+    </div>
+    <div class="lil__contents-products">
+      <h2 class="lil__contents-products--title">Dein Testpaket enthält</h2>
+      <div class="lil__contents-products--items">
+        <div class="lil__product">
+          <img class="lil__product--img" src=""/>
+          <span class="lil__product--title">10 LILLYDOO WINDELN</span>
+          <span class="lil__product--description">
+            <ul>
+              <li>0 % Parfüme & Lotionen, 100 % LILLYDOO Schutz</li>
+              <li>Extra weich und mit idealer Passform</li>
+            </ul>
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
