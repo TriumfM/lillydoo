3276a8865679b0a0c20b75b1a3785a161a89a4b5
TriumfM <triumf_96@hotmail.com>
2019-06-09 22:39:35 +0200

    logic and fix some bugs in template

---
+++ src/helpers/HeaderComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <section class="header">
+    <img class="header__certificate" src="@/assets/images/oekotex_de.png"/>
+    <div class="header__discover">
+      <span class="header__discover-label">Entdecke unsere Hautfreundlichkeit</span>
+      <a class="lil-button header__button"> Jetzt Deine Größe wählen</a>
+    </div>
+  </section>
+</template>
