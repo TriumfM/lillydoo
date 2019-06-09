2f86836f225a333cfb2bb6794c810676eb6efd45
TriumfM <triumf_96@hotmail.com>
2019-06-09 21:01:07 +0200

    complite template and responsive part

--- src/home/TastpaketComponent.vue
+++ src/home/TastpaketComponent.vue
@@ -1,27 +1,66 @@
 <template class="lil-app__content">
   <div class="lil-taskpaket">
 
-    <div class="lil-taskpaket__entry">
+    <section class="lil-taskpaket__entry">
       <img class="lil-taskpaket__certificate" src="@/assets/images/oekotex_de.png"/>
       <div class="lil-taskpaket__discover">
         <span class="lil-taskpaket__discover-label">Entdecke unsere Hautfreundlichkeit</span>
         <a class="lil-button lil-taskpaket__button"> Jetzt Deine Größe wählen</a>
       </div>
-    </div>
+    </section>
 
-    <div class="lil-taskpaket__main-product">
-      <div class="lil__main-product--photo">
-        <img class="lil__main-product--img" src=""/>
+    <section class="lil-taskpaket__main-product">
+      <div class="lil-taskpaket__main-product--photo">
+        <img class="lil-taskpaket__main-product--photo-img" src="@/assets/images/lightblue_lightblue.jpg"/>
+        <img class="lil-taskpaket__main-product--photo-certificate" src="@/assets/images/oekotex_de.png"/>
       </div>
-      <div class="lil__main-product--description">
-        <span class="lil__description--title"></span>
-        <div class="lil__description--buttons"></div>
-        <div class="lil__description--content"></div>
-        <button class="lil__button lil__main-product--button">IN DEN WARENKORB LEGEN</button>
+      <div class="lil-taskpaket__main-product--description">
+        <div class="details">
+          <h2 class="details__title">Unser gratis Testpaket</h2>
+          <div class="details__button-section">
+            <span class="details__button-section--title">WÄHLE DEINE GRÖSSE</span>
+            <ul class="details__button-section--buttons">
+              <li class="lil__main-product--button">
+                <p class="order">1</p>
+                <p class="type">(2-3 KG)</p>
+              </li>
+              <li class="lil__main-product--button">
+                <p class="order">2</p>
+                <p class="type">(3-4 KG)</p>
+              </li>
+              <li class="lil__main-product--button">
+                <p class="order">3</p>
+                <p class="type">(4-7 KG)</p>
+              </li>
+              <li class="lil__main-product--button">
+                <p class="order">4</p>
+                <p class="type">(7-10 KG)</p>
+              </li>
+              <li class="lil__main-product--button">
+                <p class="order">5</p>
+                <p class="type">(10-12 KG)</p>
+              </li>
+            </ul>
+          </div>
+          <div class="details__content">
+            <span class="details__content--summary">
+              Teste jetzt unsere Windeln und Feuchttücher. Wir zahlen dieProdukte, Du nur den Versand.
+            </span>
+            <ul class="details__content--list">
+              <li class="details__content--list-item lil-app__link lil-cursor__default">Automatischer Übergang ins jederzeit kündbare Windel-Abo
+                für 49,50 € pro Lieferung.
+              </li>
+              <li class="details__content--list-item">
+                Preise inkl. MwSt., ggf. zzgl. Versandkosten.
+              </li>
+            </ul>
+          </div>
+          <button class="lil-button details--button">IN DEN WARENKORB LEGEN</button>
+        </div>
       </div>
-    </div>
+    </section>
 
-    <div class="lil-taskpaket__functions">
+    <section class="lil-taskpaket__functions">
       <h2 class="lil-taskpaket__functions--title lil-align lil-align--h-center">Wie es funktioniert</h2>
       <div class="lil-taskpaket__functions--items">
         <div class="lil__item">
@@ -54,22 +93,40 @@
           <a class="lil__item--link"></a>
         </div>
       </div>
-    </div>
-    <div class="lil-taskpaket__contents-products">
-      <h2 class="lil__contents-products--title">Dein Testpaket enthält</h2>
-      <div class="lil__contents-products--items">
+    </section>
+    <section class="lil-taskpaket__products">
+      <h2 class="lil-taskpaket__products--title lil-align lil-align--h-center">Dein Testpaket enthält</h2>
+      <div class="lil-taskpaket__products--items">
         <div class="lil__product">
-          <img class="lil__product--img" src=""/>
-          <span class="lil__product--title">10 LILLYDOO WINDELN</span>
-          <span class="lil__product--description">
+          <div class="lil__product--img">
+            <img src="@/assets/images/windeln.jpg"/>
+          </div>
+          <div class="lil__product--description">
+            <span class="lil__product--description-title">10 LILLYDOO WINDELN</span>
+            <div class="lil__product--description-content">
             <ul>
               <li>0 % Parfüme & Lotionen, 100 % LILLYDOO Schutz</li>
               <li>Extra weich und mit idealer Passform</li>
             </ul>
-          </span>
+          </div>
+          </div>
+        </div>
+        <div class="lil__product">
+          <div class="lil__product--img">
+            <img src="@/assets/images/sensitive-wipes_one.jpg"/>
+          </div>
+          <div class="lil__product--description">
+            <span class="lil__product--description-title">10 LILLYDOO WINDELN</span>
+            <div class="lil__product--description-content">
+              <ul>
+                <li>0 % Parfüme & PEGs, 100 % biologisch abbaubar</li>
+                <li>Extra dickes und kompostierbares Tuch</li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   </div>
 </template>
 
